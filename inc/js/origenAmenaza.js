2e8eac22ed30b9d86b8b40b1410384dcebfc9abc
factoria <Carlos Arroyo Villalobos>
2018-08-08 17:44:11 -0600

    Merge origin/master
    Conflicts:
    	index.php

--- inc/js/origenAmenaza.js
+++ inc/js/origenAmenaza.js
@@ -158,20 +158,25 @@ function active_origen_action(id,activo) {
 }
 
 
-
+//ACTUALIZAR ESTE METODO
 function update_origen_amenaza(id){
      if (validate_capitulo()) {
         var loading = document.getElementById('loading_container');
         loading.innerHTML = cargando_bar;
         //Obtener Valores
-        var titulo = document.getElementById('capitulo_title').value;          
-        var descripcion = CKEDITOR.instances['capitulo_Descripcion'].getData();
+        var nombre = document.getElementById('nombre').value;          
+        var activo = 0;
+        if (document.getElementById('inlineCheckbox1').checked)
+            activo = 1;
+        else
+            activo = 0;
+        
         var ajax = NuevoAjax();
         var _values_send =
                 'id=' + id +
-                '&titulo=' + titulo +              
-                '&descripcion=' + descripcion;
-        var _URL_ = "mod/adminPlanEmergencia/adminCapitulos/ajax_edit_capitulo.php?";
+                '&nombre=' + nombre +              
+                '&activo=' + activo;
+        var _URL_ = "mod/adminPlanEmergencia/adminMatriz/adminOrigenAmenaza/ajax_edit_origen_amenaza.php?";
         //alert(_URL_ + _values_send); //DEBUG
         ajax.open("GET", _URL_ + _values_send, true);
         ajax.onreadystatechange = function () {
@@ -183,7 +188,7 @@ function update_origen_amenaza(id){
                 //alert(response); //DEBUG
                 if (response == 0) {
                     jAlert("Capitulo actualizado con exito", "Exito");
-                    OpcionMenu('mod/adminPlanEmergencia/adminCapitulos/list_capitulos.php?', '');
+                    OpcionMenu('adminPlanEmergencia/adminMatriz/adminOrigenAmenaza/list_origen_amenaza.php?', '');
                 } else if (response == 1 || response == 2) {
                     jAlert("Error en la Base de Datos, intente nuevamente.\n Si persiste informe a la USTDS", "Error");
                 } else if (response == 3) {
