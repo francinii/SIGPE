f253e2475b3e3224f55c6df4739ce2d270819175
Francini <francini113@gmail.com>
2018-08-15 19:38:48 -0600

    Merge origin/master
    Conflicts:
    	src/gafe/vista/ControlFormularioPrincipal.java
    	src/recursos/config.txt

--- mod/planEmergencia/plan_emergencia_matriz.php
+++ mod/planEmergencia/plan_emergencia_matriz.php
@@ -18,18 +18,22 @@ function consultaTipos($idOrigen) {
     return "SELECT id, descripcion, FkidOrigen FROM TipoAmenaza where FkidOrigen=$idOrigen";
 }
 
+function consultaCategoriasPorTipo($idTipo) {
+    return "SELECT  id, descripcion, FKidTipoAmenaza,isActivo FROM CategoriaTipoAmenaza where FKidTipoAmenaza=$idTipo";
+}
+
 //Selecciona todas las categorias
 //SELECT origen.id as idOrigen, tipo.id as idTipo, categoria.id as idCategoria, tipo.FkidOrigen as fkOrigen,  origen.descripcion as origenDes,tipo.descripcion as tipoDes, categoria.descripcion as categoriaDes  FROM
 //(SELECT  id, descripcion, isActivo, FkidOrigen FROM TipoAmenaza) tipo,(SELECT  id, descripcion  FROM OrigenAmenaza) origen,
 //(SELECT  id, descripcion, FKidTipoAmenaza FROM CategoriaTipoAmenaza) categoria
 //where tipo.FkidOrigen = origen.id and tipo.id = categoria.FKidTipoAmenaza
 //Selecciona las categorias correspondientes a un tipo
-function consultaCategoriasPorTipo($idTipo) {
-    return "SELECT origen.id as idOrigen, tipo.id as idTipo, categoria.id as idCategoria, tipo.FkidOrigen as fkorigen, categoria.FKidTipoAmenaza as FKidTipoAmenaza,  origen.descripcion as origenDes,tipo.descripcion as tipoDes, categoria.descripcion as categoriaDes  FROM
-(SELECT  id, descripcion, isActivo, FkidOrigen FROM TipoAmenaza) tipo,(SELECT  id, descripcion,isActivo  FROM OrigenAmenaza) origen,
-(SELECT  id, descripcion, FKidTipoAmenaza,isActivo FROM CategoriaTipoAmenaza) categoria
-where tipo.FkidOrigen = origen.id and tipo.id = categoria.FKidTipoAmenaza and categoria.FKidTipoAmenaza = $idTipo";
-}
+//function consultaCategoriasPorTipo($idTipo) {
+//    return "SELECT origen.id as idOrigen, tipo.id as idTipo, categoria.id as idCategoria, tipo.FkidOrigen as fkorigen, categoria.FKidTipoAmenaza as FKidTipoAmenaza,  origen.descripcion as origenDes,tipo.descripcion as tipoDes, categoria.descripcion as categoriaDes  FROM
+//(SELECT  id, descripcion, isActivo, FkidOrigen FROM TipoAmenaza) tipo,(SELECT  id, descripcion,isActivo  FROM OrigenAmenaza) origen,
+//(SELECT  id, descripcion, FKidTipoAmenaza,isActivo FROM CategoriaTipoAmenaza) categoria
+//where tipo.FkidOrigen = origen.id and tipo.id = categoria.FKidTipoAmenaza and categoria.FKidTipoAmenaza = $idTipo";
+//}
 
 function consultaCategoriasPorOrigen($idOrigen) {
     return "SELECT origen.id as idOrigen, tipo.id as idTipo, categoria.id as idCategoria, tipo.FkidOrigen as fkorigen,  origen.descripcion as origenDes,tipo.descripcion as tipoDes, categoria.descripcion as categoriaDes  FROM
@@ -49,7 +53,7 @@ $sql = "SELECT COUNT(id) AS cant
         FROM ZonaTrabajo";
 
 //$origenes = seleccion(consultaOrigenes());
-$tipos = seleccion(consultaTipos());
+//$tipos = seleccion(consultaTipos());
 $origenes = seleccion(consultaOrigenes());
 //
 //$find_key = (isset($_GET['find_key'])) ? $_GET['find_key'] : '';
@@ -115,43 +119,48 @@ $origenes = seleccion(consultaOrigenes());
             <?php
             for ($i = 0; $i < count($origenes); $i++) {
                 $idOrigen = $origenes[$i]['id'];
-                $categorias = seleccion(consultaCategoriasPorOrigen($idOrigen));
+                $categorias = seleccion(consultaCategoriasPorOrigen($idOrigen)); //cambiar esta consulta
                 $cantidadCategorias = count($categorias);
                 $tipos = seleccion(consultaTipos($idOrigen));
                 $cantidadTipos = count($tipos);
-                ?>       
-                <tr>
-                    <td rowspan="<?= $cantidadCategorias ?>"> <?= $origenes[$i]['descripcion']; ?> </td>
-                    <?php for ($j = 0; $j < $cantidadTipos; $j++) { ?>
-                        <?php
-                        $idTipo = $tipos[$j]['id']; 
-                        $categoriasPorTipo = seleccion(consultaCategoriasPorTipo($idTipo));
-                        $cantidadCategoriasPorTipo = count($categoriasPorTipo);
-                        ?>
-                        <?php if ($j != 0) { ?>
-                        <tr>
-                        <?php } ?>
-                        <td rowspan="<?= $cantidadCategoriasPorTipo ?>"> <?= $tipos[$j]['descripcion']; ?> </td>
-                        <?php for ($k = 0; $k < $cantidadCategoriasPorTipo; $k++) { ?>
-                            <?php if ($k != 0 && $j != 0) { ?>
+                ?>
+                <?php // if ($origenes > 0) { ?>
+                <?php if ($cantidadCategorias > 0) { ?>
+                    <tr>
+                        <td rowspan="<?= $cantidadCategorias ?>"> <?= $origenes[$i]['descripcion']; ?> </td>
+                        <?php for ($j = 0; $j < $cantidadTipos; $j++) { ?>
+                            <?php
+                            $idTipo = $tipos[$j]['id'];
+                            $categoriasPorTipo = seleccion(consultaCategoriasPorTipo($idTipo));
+                            $cantidadCategoriasPorTipo = count($categoriasPorTipo);
+                            ?>
+                            <?php if ($j != 0 && $cantidadCategoriasPorTipo > 0) { ?>
                             <tr>
                             <?php } ?>
-                            <td> <?= $categorias[$j]['descripcion']; ?></td>
-                            <td> fuente</td>
-                            <td> valor</td>
-                            <td> criterio</td>
-                            <td> valor</td>
-                            <td> criterio</td>
-                            <td> valor</td>
-                            <td> criterio</td>
-                            <td> valor</td>
-                            <td> criterio</td>
-                        </tr>
+                            <?php if ($cantidadCategoriasPorTipo > 0) { ?>
+                                <td rowspan="<?= $cantidadCategoriasPorTipo ?>"> <?= $tipos[$j]['descripcion']; ?> </td>
+                                <?php for ($k = 0; $k < $cantidadCategoriasPorTipo; $k++) { ?>
+                                    <?php if ($k != 0 && $j != 0) { ?>
+                                    <tr>
+                                    <?php } ?>
+                                    <td> <?= $categoriasPorTipo[$k]['descripcion']; ?></td>
+                                    <td> fuente</td>
+                                    <td> valor</td>
+                                    <td> criterio</td>
+                                    <td> valor</td>
+                                    <td> criterio</td>
+                                    <td> valor</td>
+                                    <td> criterio</td>
+                                    <td> valor</td>
+                                    <td> criterio</td>
+                                </tr>
+                            <?php } ?>
+                        <?php } else { ?>
+                            </tr>
+                        <?php } ?>
                     <?php } ?>
-
-                <?php } ?>
+                <?php } ?>  
             <?php } ?>
-
         </tbody>
     </table>
 
