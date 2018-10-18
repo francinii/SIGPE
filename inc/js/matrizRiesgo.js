3234cb2bcc8b976920d5c95ba146bc97cc1e0527
factoria <Carlos Arroyo Villalobos>
2018-10-18 09:45:37 -0600

    Merge origin/master
    Conflicts:
    	mod/planEmergenciaPDF/planEmergenciaPDF.php

--- inc/js/matrizRiesgo.js
+++ inc/js/matrizRiesgo.js
@@ -1,5 +1,11 @@
-//Selector que cambia el valor del criterio
-
+/*
+ * Funcion que cambia el valor del criterio al hacer onchange en el selector
+ * @param {event} event recibe el evento onchange del selector de probabilidad
+ * o de gravedad o de consecuencia
+ * @param {int} cod  corresponde al valor que determina si el selector es el selector
+ * de probabilidad o el de consecuencia/gravedad: probabilidad (0) o consecuecia/gravedad (1)
+ * @returns {undefined} Llama al método calcularTipoAlerta
+ */
 function cambiarCriterio(event, cod) {
     var opcion = event.value; //selector
     var hermano = event.parentNode.nextElementSibling; //td hermano al td del selector
@@ -11,6 +17,12 @@ function cambiarCriterio(event, cod) {
     calcularTipoAlerta(event);
 }
 
+/*
+ * Funcion que cambia el criterio al hacer onchange en el selector de valor de probabilidad
+ * @param {int} opc recibe el valor del selector de probabilidad
+ * @returns {String} retorna el criterio de la probabilidad: BAJA MEDIA o ALTA. En caso
+ * de que opc no sea 1,2 o 3 retorna "".
+ */
 function selectorProbabilidad(opc) {
     if (opc == 1) {
         return "BAJA";
@@ -22,6 +34,13 @@ function selectorProbabilidad(opc) {
     return "";
 }
 
+/*
+ * Funcion que cambia el criterio al hacer onchange en el selector de valor Gravedad
+ * o consecuencia.
+ * @param {int} opc recibe el valor del selector de de gravedad o consecuena
+ * @returns {String} retorna el criterio de la probabilidad: NINGUNA, BAJA, MEDIA o ALTA. En caso
+ * de que opc no sea 1,2, 3 o 4 retorna "".
+ */
 function selectorGravedadConsecuencia(opc) {
     if (opc == 1) {
         return "NINGUNA";
@@ -35,12 +54,12 @@ function selectorGravedadConsecuencia(opc) {
     return "";
 }
 
-
-function obtenerCriterioAlerta() {
-    // var tr = JQuery("#matriz_riesgos tbody")
-    //var criterioTipoAlerta = tr.lastElementChild;
-}
-
+/*
+ * Funcion que calcula el valor tipo de alerta 
+ * @param {event} event recibe el evento onchange del selector de probabilidad
+ * o de gravedad o de consecuencia de amenaza
+ * @returns {undefined} Llama al método calcularCriterioTipoAlerta
+ */
 function calcularTipoAlerta(event) {
     var opcion = event.value;
     var tr = event.parentNode.parentNode; //td hermano al td del selector
@@ -57,6 +76,15 @@ function calcularTipoAlerta(event) {
     calcularCriterioTipoAlerta(criterioTipoAlerta, valorTipoAlerta);
 }
 
+/*
+ * Funcion que calcula el criterio del tipo de alerta y cambia el color al td de 
+ * criterio del tipo de alerta.
+ * @param {String} criterioTipoAlerta: corresponde al Elemento (td) Criterio del tipo
+ * de alerta.
+ * @param {int} valor recibe el valor del Tipo de alerta para el calculo del 
+ * criterio del tipo de alerta.
+ * @returns {undefined}
+ */
 function calcularCriterioTipoAlerta(criterioTipoAlerta, valor) {
     var valorTipoAlerta = valor.innerHTML;
     if (valorTipoAlerta <= 3) {
@@ -75,19 +103,33 @@ function calcularCriterioTipoAlerta(criterioTipoAlerta, valor) {
     }
 }
 
-//Crea un vector con los datos de la tabla correspondiente a los colores de la 
-//matriz
+
+/*
+ * Funcion que crea un vector con los datos de la tabla correspondiente a los
+ * colores de la matriz.
+ * @returns {Vector} retorna un vector con los criterio de los tipos de alerta 
+ * de la matriz
+ */
 function crearVectorValores() {
     var criterio = [];
     jQuery("td.criterioAlerta").each(function () {
         criterio.push(jQuery(this).text());
     }).get();
     return criterio;
-
 }
 
-//Crea un vector con los datos de la tabla correspondiente a los colores de la 
-//matriz
+/*
+ * Funcion que genera un arreglo con todos los registros de la matriz. y envia los
+ * datos por ajax
+ * @param {String} nombreCentro: corresponde al nombre del centro de trabajo
+ * @param {String} idCentro corresponde al id del centro
+ * @param {int} indica la pagina a la que sera redirigido.Pueden ser los valores
+ * 1 o 2 
+ * @param {String} idCentro corresponde al id del centro
+ * @param {String} editar estado en que esta la pagina 
+ * @returns {Undefined} 
+ * de la matriz
+ */
 function generaVectorMatriz(nombreCentro, idCentro, clave, editar) {
    if(editar){
     var matriz = jQuery("#matriz_riesgos tbody tr");
--- mod/planEmergencia/plan_emergencia_matriz.php
+++ mod/planEmergencia/plan_emergencia_matriz.php
@@ -3,6 +3,7 @@
  * Matriz de Riesgo administrable
  * 
  */
+
 include("../login/check.php");
 include("../../functions.php");
 $vocab = $mySessionController->getVar("vocab");
@@ -13,8 +14,6 @@ $user_rol = $mySessionController->getVar("rol");
 $ip = $mySessionController->getVar("cds_domain");
 $ip .= $mySessionController->getVar("cds_locate");
 $page_cant = $mySessionController->getVar("page_cant");
-
-
 include("plan_emergencia_menu.php");
 
 /**
@@ -25,20 +24,22 @@ function consultaOrigenes() {
     return "SELECT id, descripcion, isActivo FROM OrigenAmenaza where isActivo = 1";
 }
 
-
 /*
  * selecciona los tipos de amenaza
  * @param {int} $idOrigen id del origen de la amenaza
  * @returns {array} 
  */
+
 function consultaTipos($idOrigen) {
     return "SELECT id, descripcion, FkidOrigen, isActivo FROM TipoAmenaza where FkidOrigen=$idOrigen and isActivo = 1";
 }
+
 /*
  * selecciona los categorias de amenaza por tipo
  * @param {int} $idTipo id del tipo de la amenaza
  * @returns {array} 
  */
+
 function consultaCategoriasPorTipo($idTipo) {
     return "SELECT  id, descripcion, FKidTipoAmenaza,isActivo FROM CategoriaTipoAmenaza where FKidTipoAmenaza=$idTipo and isActivo = 1";
 }
@@ -48,6 +49,7 @@ function consultaCategoriasPorTipo($idTipo) {
  * @param {int} $idOrigen id del origen de la amenaza
  * @returns {array} 
  */
+
 function consultaCategoriasPorOrigen($idOrigen) {
     return "SELECT  categoria.id as idCategoria FROM
 (SELECT  id, FkidOrigen FROM TipoAmenaza where isActivo = 1 ) tipo,
@@ -63,6 +65,7 @@ where tipo.FkidOrigen = origen.id and tipo.id = categoria.FKidTipoAmenaza and ti
  * @param {int} $editar  estado en que esta la pagina 
  * @returns {String} HTML
  */
+
 function selectorMatriz($cod, $opcion, $editar) {
     $valor = '<select    ' . (($editar) ? '' : 'disabled') . ' class="form-control cambios" onchange = "javascript:cambiarCriterio(this,' . $cod . ')">';
     $valor .= '  <option ' . (($opcion == 1) ? 'selected' : '') . '> 1</option>';
@@ -75,6 +78,15 @@ function selectorMatriz($cod, $opcion, $editar) {
     return $valor;
 }
 
+/*
+ * Funcion que busca un registro de una categoria especifica en la lista de 
+ * registros de la matriz
+ * @param {array} $matriz arreglo con los registros de la matriz
+ * @param {int} id de la categoria a buscar en el array $matriz
+ * @returns {Array} $mat array asociativo con keys "fuente", "probabilidad",
+ *  "gravedad", "consecuenaAmenaza" en caso de no encontrarse retorna valores 
+ * por defecto
+ */
 
 function buscarRegistro($matriz, $categoria) {
     for ($i = 0; $i < count($matriz); $i++) {
@@ -86,13 +98,12 @@ function buscarRegistro($matriz, $categoria) {
     return $mat = ["fuente" => "", "probabilidad" => 1, "gravedad" => 1, "consecuenciaAmenaza" => 1];
 }
 
-function buscar($color, $criterios) {
-    $total = 0;
-    foreach ($criterios as $criterio) {
-        $total = ($criterio == $color) ? $total + 1 : $total;
-    }
-    return $total;
-}
+/*
+ * Funcion que determina el criterio de la probabilidad o gravedad de acuerdo al
+ * valor de probabilidad y gravedad
+ * @param {int} $opc corresponde al valor de  probabilidad o gravedad
+ * @returns {String} HTML
+ */
 
 function selectorProbabilidad($opc) {
     if ($opc == 1) {
@@ -104,6 +115,13 @@ function selectorProbabilidad($opc) {
     }
     return "";
 }
+
+/*
+ * Funcion que determina el criterio de la consecuencia de amenaza de acuerdo al
+ * valor de  de la consecuencia de amenaza 
+ * @param {int} $opc corresponde a la consecuencia de amenaza
+ * @returns {String} HTML
+ */
 
 function selectorGravedadConsecuencia($opc) {
     if ($opc == 1) {
@@ -118,9 +136,27 @@ function selectorGravedadConsecuencia($opc) {
     return "";
 }
 
+/*
+ * Funcion que calcula el valor correspondiente al tipo de alerta 
+ * @param {array} $registros corresponde a un arreglo asociativo que posee los valores
+ * de la fila de la matriz (probabilidad, gravedad, consecuenciaAmenaza)
+ * @returns {String} HTML
+ */
+
 function calcularValorAlerta($registro) {
     return $registro['probabilidad'] * ( $registro['gravedad'] + $registro['consecuenciaAmenaza'] );
 }
+
+/*
+ * Funcion que calcula el criterio correspondiente al tipo de alerta. Si el valor ($valor)del
+ * tipo de alerta es menor o igual a 3 el criterio es "NINGUNA", si es mayor que 3
+ * y menor o igual a 12 el criterio es "VERDE", si es mayor que 12 y menor o igual que 24 el 
+ * criterio es "AMARILLA" y si es mayor o igual que 24, el criterio es "ROJA"
+ * @param {array} $registros corresponde a un arreglo asociativo que posee los valores
+ * de la fila de la matriz (probabilidad, gravedad, consecuenciaAmenaza)
+ * @param {array} $vocab corresponde al vocabulario del sistema
+ * @returns {array} $mat array asociativo con keys "criterio", "color"
+ */
 
 function calcularCriterioAlertaColor($registro, $vocab) {
     $valor = calcularValorAlerta($registro);
@@ -140,6 +176,15 @@ $sql = "SELECT id, FKidCategoriaTipoAmenaza, FKidZonaTrabajo, fuente,
  probabilidad,gravedad,consecuenciaAmenaza FROM Matriz 
 WHERE `FKidZonaTrabajo`=" . $idPlanEmergencia;
 $matriz = seleccion($sql);
+
+
+//function buscar($color, $criterios) {
+//    $total = 0;
+//    foreach ($criterios as $criterio) {
+//        $total = ($criterio == $color) ? $total + 1 : $total;
+//    }
+//    return $total;
+//}
 ?>
 
 <div class="well well-sm">
@@ -180,9 +225,8 @@ $matriz = seleccion($sql);
                 $tipos = seleccion(consultaTipos($idOrigen));
                 $cantidadTipos = count($tipos);
                 ?>
-                <?php // if ($origenes > 0) { ?>
-
-                <?php if ($cantidadCategorias > 0) { ?>
+                <?php // if ($origenes > 0) {  ?>
+    <?php if ($cantidadCategorias > 0) { ?>
                     <tr>
                         <td rowspan="<?= $cantidadCategorias ?>"> <?= $origenes[$i]['descripcion']; ?> </td>
                         <?php for ($j = 0; $j < $cantidadTipos; $j++) { ?>
@@ -201,12 +245,12 @@ $matriz = seleccion($sql);
                                 <?php for ($k = 0; $k < $cantidadCategoriasPorTipo; $k++) { ?>
                                     <?php if ($k != 0 && $j != 0) { ?>
                                     <tr>
-                                    <?php } ?>                                   
+                    <?php } ?>                                   
                                     <td class ="categoria"> <?= $categoriasPorTipo[$k]['descripcion']; ?>
                                         <input   type="hidden" class ="idCategoria" value="<?= $categoriasPorTipo[$k]['id']; ?>" />
                                     </td>
 
-                                    <?php $registroMatriz = buscarRegistro($matriz, $categoriasPorTipo[$k]['id']); ?>  
+                    <?php $registroMatriz = buscarRegistro($matriz, $categoriasPorTipo[$k]['id']); ?>  
                                     <td> <div class="form-group">                                            
                                             <input <?= (!$editar) ? "disabled" : ""; ?> id="type-text" name="type-text" class="form-control fuente cambios" placeholder="fuente"value ="<?= $registroMatriz['fuente'] ?>"  title="propiedad title" type="text">
                                         </div>
@@ -232,22 +276,20 @@ $matriz = seleccion($sql);
                         <?php } ?>
                     <?php } $bandera = 0; ?>
                 <?php } ?>  
-            <?php } ?>
+<?php } ?>
         </tbody>
     </table>
     <br/>
-
-
 </div>
 <br>
 <div class="text-center">
     <?php if ($criterios != null) { ?> 
     <?php if ($editar) { ?>  
-        <span class="text-center"><a class="btn btn-warning" name="submit" onclick="javascript:generaVectorMatriz('<?= $nombreCentro ?>', '<?= $idCentro ?>', '0', <?= $editar ?>);"><i class='fa fa-plus fa-inverse'></i> <?= $vocab["symbol_save"] ?> <?= $vocab["matriz_title"] ?></a></span>
-        <span class="text-center"><a class="btn btn-success" name="submit" onclick="javascript:generaVectorMatriz('<?= $nombreCentro ?>', '<?= $idCentro ?>', '2', <?= $editar ?>);"><i class='fa fa-rotate-right fa-inverse'></i> <?= $vocab["symbol_save"] ?> <?= $vocab["datos_generares_siguente"] ?></a></span>
+            <span class="text-center"><a class="btn btn-warning" name="submit" onclick="javascript:generaVectorMatriz('<?= $nombreCentro ?>', '<?= $idCentro ?>', '0', <?= $editar ?>);"><i class='fa fa-plus fa-inverse'></i> <?= $vocab["symbol_save"] ?> <?= $vocab["matriz_title"] ?></a></span>
+            <span class="text-center"><a class="btn btn-success" name="submit" onclick="javascript:generaVectorMatriz('<?= $nombreCentro ?>', '<?= $idCentro ?>', '2', <?= $editar ?>);"><i class='fa fa-rotate-right fa-inverse'></i> <?= $vocab["symbol_save"] ?> <?= $vocab["datos_generares_siguente"] ?></a></span>
     <?php } ?>
-      
+
         <span class="text-center"><a class="btn btn-warning" name="" onclick="javascript:generaVectorMatriz('<?= $nombreCentro ?>', '<?= $idCentro ?>', '1', <?= $editar ?>);"><i class='fa fa-plus fa-inverse'></i>  <?= $vocab["graficar_matriz"] ?></a></span>
 
-    <?php } ?>
+<?php } ?>
 </div>
